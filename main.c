4de9d594b0c37ebc8bd7bffb957d4de5b7a2e743
legale <legale.legale@gmail.com>
2022-08-15 03:38:38 -0400

    minor optimization
    Signed-off-by: legale <legale.legale@gmail.com>

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 #define ENTRY(x) {x, #x}
 #define STRING_M(x) #x
 
+/* rtnetlink route netlink attributes buffer */
+static struct rtattr *tb[NDA_MAX + 1];
+
 static void parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, int len) {
     /* loop over all rtattributes */
     while (RTA_OK(rta, len) && max--) {
@@ -19,7 +22,7 @@ static void parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, int l
 
 int main(int argc, char **argv) {
     int status;
-    void *p; //just a ptr
+    void *p, *lp; //just a ptrs
     
     /* open socket */
     int sd = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_ROUTE);
@@ -45,25 +48,23 @@ int main(int argc, char **argv) {
     /* get an answer */
     status = recv(sd, buf, sizeof(buf), 0);
     int buf_size = status; /* recv will return answer size */
-    p = (void *) buf; /* set p to start of an answer */
+    p = buf; /* set p to start of an answer */
+    lp = buf + buf_size; /* answer last byte ptr */
 
-    while (buf_size > 0) { /* loop while buffer size is more than 0 */
-        struct nlmsghdr *answer = (struct nlmsghdr *) p; /* netlink header structure */
+    while (p < lp) { /* loop till the end */
+        struct nlmsghdr *answer = p; /* netlink header structure */
 
-        int len = answer->nlmsg_len; /* netlink message length including header */
+        uint32_t len = answer->nlmsg_len; /* netlink message length including header */
         struct ndmsg *msg = NLMSG_DATA(answer); /* macro to get a ptr right after header */
         /* Given the payload length, len, this macro returns the aligned
          * length to store in the nlmsg_len field of the nlmsghdr.
          * */
-        int msg_len = NLMSG_LENGTH(sizeof(*msg));
+        uint32_t msg_len = NLMSG_LENGTH(sizeof(*msg));
         len -= msg_len; /* count message length left */
         p += msg_len; /* move ptr forward */
 
-        /* rtnetlink route netlink attributes buffer */
-        struct rtattr *tb[NDA_MAX + 1] = {0};
-
         /* this is very first rtnetlink attribute */
-        struct rtattr *rta = (struct rtattr *) p;
+        struct rtattr *rta = p;
         memset(tb, 0, sizeof(tb)); /* clear attribute buffer */
         parse_rtattr(tb, NDA_MAX, rta, len); /* fill tb attribute buffer */
         if (tb[NDA_DST]) { /* this is destination address */
@@ -79,8 +80,7 @@ int main(int argc, char **argv) {
             fprintf(stdout, "lladdr: \n");
         }
 
-        p += len;
-        buf_size -= answer->nlmsg_len;
+        p += len; /* move ptr to the next netlink message */
     }
 
     return 0;
