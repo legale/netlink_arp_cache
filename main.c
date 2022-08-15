65359e84978dfc9f670e02cd36eb9d1a06edcb9e
legale <legale.legale@gmail.com>
2022-08-15 16:21:17 -0400

    minor refactoring
    Signed-off-by: legale <legale.legale@gmail.com>

--- main.c
+++ main.c
@@ -7,20 +7,15 @@
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
 
-
-#define ENTRY(x) {x, #x}
-#define STRING_M(x) #x
-
 /* cli arguments parse macro and functions */
 #define NEXT_ARG() do { argv++; if (--argc <= 0) incomplete_command(); } while(0)
 #define NEXT_ARG_OK() (argc - 1 > 0)
 #define PREV_ARG() do { argv--; argc++; } while(0)
 
 static char *argv0; /* ptr to the program name string */
-static void usage(void)
-{
+static void usage(void) {
     fprintf(stdout,
-            "Usage:   %s [option] [value]         \n"
+            "Usage:   %s [option] [value]   \n"
             "            option: ip | mac | help  \n"
             "\n"
             "Example: %s ip 192.168.1.1           \n"
@@ -31,8 +26,7 @@ static void usage(void)
 
 /* Returns true if 'prefix' is a not empty prefix of 'string'.
  */
-static bool matches(const char *prefix, const char *string)
-{
+static bool matches(const char *prefix, const char *string) {
     if (!*prefix)
         return false;
     while (*string && *prefix == *string) {
@@ -43,8 +37,7 @@ static bool matches(const char *prefix, const char *string)
     return !*prefix;
 }
 
-static void incomplete_command(void)
-{
+static void incomplete_command(void) {
     fprintf(stderr, "Command line is not complete. Try option \"help\"\n");
     exit(-1);
 }
@@ -52,12 +45,12 @@ static void incomplete_command(void)
 /* rtnetlink route netlink attributes buffer */
 static struct rtattr *tb[NDA_MAX + 1];
 
-static void parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, int len) {
+static void parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, unsigned len) {
     /* loop over all rtattributes */
     while (RTA_OK(rta, len) && max--) {
         tb[rta->rta_type] = rta; /* store attribute ptr to the tb array */
         rta = RTA_NEXT(rta, len); /* special rtnetlink.h macro to get next netlink route attribute ptr */
-    };
+    }
 }
 
 int main(int argc, char **argv) {
@@ -68,15 +61,15 @@ int main(int argc, char **argv) {
     argv0 = *argv; /* set program name */
     while (argc > 1) {
         NEXT_ARG();
-        if (matches(*argv, "ip")){
+        if (matches(*argv, "ip")) {
             mode = 1;
             NEXT_ARG();
             val_search = *argv;
-        } else if (matches(*argv, "mac")){
+        } else if (matches(*argv, "mac")) {
             mode = 2;
             NEXT_ARG();
             val_search = *argv;
-        } else if (matches(*argv, "help")){
+        } else if (matches(*argv, "help")) {
             usage();
         } else {
             usage();
@@ -84,7 +77,7 @@ int main(int argc, char **argv) {
         argc--; argv++;
     }
 
-    int status;
+    int64_t status;
     void *p, *lp; //just a ptrs
 
     /* open socket */
@@ -104,13 +97,16 @@ int main(int argc, char **argv) {
 
     /* send request */
     status = send(sd, &req, req.n.nlmsg_len, 0);
+    if (!status) fprintf(stdout, "error: socket send return %ld\n", status); /* check send status */
 
     /* this is buffer to store an answer */
-    char buf[16384] = {0};
+    char buf[8192];
 
     /* get an answer */
     status = recv(sd, buf, sizeof(buf), 0);
-    int buf_size = status; /* recv will return answer size */
+    if (!status) fprintf(stdout, "error: socket recv return %ld\n", status); /* check recv status */
+
+    int64_t buf_size = status; /* recv will return answer size */
     p = buf; /* set p to start of an answer */
     lp = buf + buf_size; /* answer last byte ptr */
 #ifdef DEBUG
@@ -122,7 +118,7 @@ int main(int argc, char **argv) {
 
         struct ndmsg *msg = NLMSG_DATA(answer); /* macro to get a ptr right after header */
         /* skip broadcast entries */
-        if(msg->ndm_state & NUD_NOARP){
+        if (msg->ndm_state & NUD_NOARP) {
             p += len;
             continue;
         }
@@ -155,7 +151,7 @@ int main(int argc, char **argv) {
                     addr_raw[0], addr_raw[1], addr_raw[2], addr_raw[3], addr_raw[4], addr_raw[5]);
         }
 
-        switch(mode) {
+        switch (mode) {
             case 1:
                 if (strcmp(val_search, ip) == 0) {
                     fprintf(stdout, "%s laddr %s\n", ip, addr);
